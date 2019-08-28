e773f12f890e8cab19e9811748734b2f0a7d4f9e
gandreottola <gandreottola09@gmail.com>
2019-08-27 23:32:28 -0400

    Working GET request for showing all cryptos

--- src/components/IndexCryptos/IndexCryptos.js
+++ src/components/IndexCryptos/IndexCryptos.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react'
-// import { Link } from 'react-router-dom'
 import { indexCryptos } from '../../api/cryptos'
 // import axios from 'axios'
 // import apiUrl from '../../apiConfig'
